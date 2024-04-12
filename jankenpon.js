757358d11b1275a4ffc0518f595c1481d8c1ad00
Oscar Finn <oscarfinn1994@gmail.com>
2024-04-12 11:29:24 +0100

    added a playRound function and score variables

--- jankenpon.js
+++ jankenpon.js
@@ -1,21 +1,56 @@
+let playerScore = 0;
+let computerScore = 0;
+let actions = ["rock","paper","scissors"];
+
 function getComputerChoice() {
     let rand = Math.floor(Math.random()*3);
-    return rand;
+    return actions[rand];
 }
 function getPlayerChoice() {
     let choice = toLowerCase(prompt("Rock, Paper or Scissors?"));
     if (choice === "rock"){
-        return 0;
+        return actions[0];
     }
     else if (choice === "paper"){
-        return 1;
+        return actions[1];
     }
     else if (choice === "scissors"){
-        return 2;
+        return actions[2];
     }
     else {
         console.log("invalid input");
         return getPlayerChoice();
     }
-    
+}
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("Draw, no points.");
+    } else if (playerSelection === "rock"){
+        if (computerSelection === "scissors") {
+            console.log("Rock beats Scissors, player wins");
+            playerScore++;
+        } else {
+            console.log("Paper beats Rock, computer wins");
+            computerScore++;
+        }
+    } else if (playerSelection==="paper") {
+        if (computerSelection === "rock") {
+            console.log("Paper beats Rock, player wins");
+            playerScore++;
+        } else {
+            console.log("Scissors beat Paper, computer wins");
+            computerScore++;
+        }
+    } else if (playerSelection==="scissors") {
+        if (computerSelection==="paper") {
+            console.log("Scissors beats Paper, player wins");
+            playerScore++;
+        } else {
+            console.log("Rock beats Scissors, computer wins");
+            computerScore++;
+        }
+    } else {
+        console.log("we shouldnt reach here");
+    }
+    console.log(`The current score is: ${playerScore}-${computerScore}`);
 }
