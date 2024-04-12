5efc842ce8661037b955d2f267ecb29a1745a198
Oscar Finn <oscarfinn1994@gmail.com>
2024-04-12 11:14:02 +0100

    added an initial get player choice function

--- jankenpon.js
+++ jankenpon.js
@@ -2,3 +2,20 @@ function getComputerChoice() {
     let rand = Math.floor(Math.random()*3);
     return rand;
 }
+function getPlayerChoice() {
+    let choice = toLowerCase(prompt("Rock, Paper or Scissors?"));
+    if (choice === "rock"){
+        return 0;
+    }
+    else if (choice === "paper"){
+        return 1;
+    }
+    else if (choice === "scissors"){
+        return 2;
+    }
+    else {
+        console.log("invalid input");
+        return getPlayerChoice();
+    }
+    
+}
